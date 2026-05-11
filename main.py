6e29c60b23e39697fb748bc9d35a6966e44ee194
lugenbfd <93534634+lugenbfd@users.noreply.github.com>
2026-05-11 19:45:50 -0400

    changed a variable name

--- main.py
+++ main.py
@@ -27,30 +27,30 @@ def create_player(player: Player):
         return players
 
 @app.get("/players", response_model=list[Player])
-def list_players(active: bool | None = None):
-    if active == None:
+def list_players(is_active: bool | None = None):
+    if is_active == None:
         return players
-    return [p for p in players if p.is_active == active]
+    return [p for p in players if p.is_active == is_active]
 
-@app.get("/players/{player_number}", response_model=Player)
-def get_player(player_number: int):
-    match = next((p for p in players if p.number == player_number), None)
+@app.get("/players/{number}", response_model=Player)
+def get_player(number: int):
+    match = next((p for p in players if p.number == number), None)
     if match == None:
         raise HTTPException(status_code=404, detail="Player not found")
     return match
 
-@app.put("/players/{player_number}", response_model=Player)
-def change_player_status(player_number: int,is_active: bool):
-    match = next((p for p in players if p.number == player_number), None)
+@app.put("/players/{number}", response_model=Player)
+def change_player_status(number: int,is_active: bool):
+    match = next((p for p in players if p.number == number), None)
     if match == None:
         raise HTTPException(status_code=404, detail="Player not found")
     match.is_active=is_active
     return match
 
-@app.delete("/players/{player_number}")
-def delete_player(player_number: int):
-    player_id = next((i for i, p in enumerate(players) if p.number == player_number), None)
+@app.delete("/players/{number}")
+def delete_player(number: int):
+    player_id = next((i for i, p in enumerate(players) if p.number == number), None)
     if player_id == None:
         raise HTTPException(status_code=404, detail="Player not found")
     players.pop(player_id)
-    return {"number": player_number, "message": "Player deleted"}
+    return {"number": number, "message": "Player deleted"}
